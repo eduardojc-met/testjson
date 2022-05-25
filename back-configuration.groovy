76191f58a904f2e99bd2a85b24f4b9bc430f056f
eduardojc-met <104899902+eduardojc-met@users.noreply.github.com>
2022-05-25 16:39:52 +0200

    Update back-configuration.groovy

--- back-configuration.groovy
+++ back-configuration.groovy
@@ -1,6 +1,54 @@
 
 def test() {
-     writeYaml file: 'kelokeeeeeeeeeeeee', data: "keloke"
+    pipeline {
+ 
+ // environment {
+   //    IBM_ACCESS_KEY_ID     = credentials('ibmuser')
+     //   IBM_SECRET_ACCESS_KEY = credentials('ibmkey')
+    //}
+ 
+ 
+  agent any
+ 
+  stages {
+ 
+    stage('Checkout Source') {
+      
+      steps {
+        git 'https://github.com/eduardojc-met/pocdeployment.git'
+      }
+    }
+ 
+ 
+ 
+    
+
+    stage('Deploying App to Kubernetes') {
+      steps {
+        script {
+
+	
+               writeYaml file: 'kelokeeeeeeeeeeeee22222', data: "keloke"
+
+         
+             }
+           
+
+
+
+
+      
+        }
+      }
+    
+
+ 
+ 
+
+ 
+  }
+  
+  }
 }
 return this
 /*
