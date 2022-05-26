34a662d412c9d44cfef87692270c5af5147636d7
eduardojc-met <104899902+eduardojc-met@users.noreply.github.com>
2022-05-26 12:31:38 +0200

    .

--- back-configuration.groovy
+++ back-configuration.groovy
@@ -15,69 +15,94 @@ def test() {
  
  
     
-    stage('Deploying App to Kubernetes') {
-     
-	
-               writeYaml file: 'x.txt', data: "xx"
+    stage('Generating app') {
+     script{
+//bat 'mvnw package -Dnative -Dquarkus.container-image.group=santander -X -Dquarkus.native.container-build=true -Dquarkus.native.resources.includes=*.p12,reflection-config.json,cacerts -Dquarkus.native.container-runtime=docker -Dquarkus.native.debug.enabled=true'
 
+     }
+	
         
             
       }
 
- 
-  
+ stage('Create & push image'){
 
+script{
+  def appVersion=readMavenPom().getVersion()
+  def appName=readMavenPom().getArtifactId()
+  bat 'docker build -f src/main/docker/Dockerfile.native -t quarkus/mgateway .'
+bat 'docker tag quarkus/mgateway ${appName}-test:${appVersion}'
 
+dir("C:/Program Files/IBM/Cloud/bin"){
+             bat label: 'Login to ibmcloud', script: '''ibmcloud.exe login -u %IBM_ACCESS_KEY_ID% -p %IBM_SECRET_ACCESS_KEY% -r eu-de ''' 
+           bat label: 'Login to ibm cr', script: '''ibmcloud.exe  cr login '''
+           bat label: 'Configuring kubernetes', script: '''ibmcloud.exe ks cluster config -c c7pb9mkf09cf7vh8tmu0
+ '''}
 }
-return this
-/*
-pipeline {
- 
- // environment {
-   //    IBM_ACCESS_KEY_ID     = credentials('ibmuser')
-     //   IBM_SECRET_ACCESS_KEY = credentials('ibmkey')
-    //}
- 
- 
-  agent any
- 
-  stages {
- 
-    stage('Checkout Source') {
-      
-      steps {
-        git 'https://github.com/eduardojc-met/pocdeployment.git'
-      }
-    }
- 
- 
- 
-    
 
+
+
+ }
+
+stage('test'){
+dir("../"){
+  bat "dir"
+}
+}
+/*
     stage('Deploying App to Kubernetes') {
       steps {
         script {
 
 	
-               writeYaml file: 'kelokeeeeeeeeeeeee', data: "keloke"
-
-         
+            def appname=readMavenPom().getArtifactId()
+            
+            def packageJSON = readJSON file: 'package.json'
+        def packageJSONVersion = packageJSON.version 
+        def pomVersion = readMavenPom().getVersion()
+             dir("src/main/docker/backend") {
+            
+              def datas = readYaml file:"back.yaml"
+               datas[0].metadata.labels=['io.kompose.service': "${appname}"+'-bff']
+               
+               
+               
+               datas[0].metadata["name"]="${appname}"+'-bff'
+               datas[0].spec.selector.matchLabels=['io.kompose.service': "${appname}"+'-bff']
+               datas[0].spec.template.spec.containers[0]["name"]="${appname}"+'-bff'
+                datas[0].spec.template.spec.containers[0]["image"]='de.icr.io/devops-tools/'+"${appname}"+'-bff:'+"${pomVersion}"
+               
+                datas[0].spec.template.metadata.labels=['io.kompose.service': "${appname}"+'-bff']
+              
+              
+              datas[1].metadata.labels=['io.kompose.service': "${appname}"+'-bff']
+              datas[1].metadata["name"]="${appname}"+'-bff'
+               datas[1].spec.selector=['io.kompose.service': "${appname}"+'-bff']
+              
+                bat 'del back.yaml'
+                writeYaml file: 'back.yaml', data: datas[0]
+              
              }
-           
-
-
-
-
+        
+            dir("C:/Program Files/IBM/Cloud/bin"){
+             bat label: 'Login to ibmcloud', script: '''ibmcloud.exe login -u %IBM_ACCESS_KEY_ID% -p %IBM_SECRET_ACCESS_KEY% -r eu-de ''' 
+           bat label: 'Login to ibm cr', script: '''ibmcloud.exe  cr login '''
+           bat label: 'Configuring kubernetes', script: '''ibmcloud.exe ks cluster config -c c7pb9mkf09cf7vh8tmu0
+ '''}
+            
+                    dir("src/main/docker/backend") {
+            bat 'kubectl apply -f back.yaml --namespace=develop'
+            bat 'kubectl apply -f serviceback.yaml --namespace=develop'
+         
+                }
+          
       
         }
       }
-    
+    }
 
- 
- 
-
- 
-  }
-  
-  }
   */
+
+
+}
+return this
