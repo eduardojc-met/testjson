9c3e7a623e193455bf3ff5810b51da8af34a6077
eduardojc-met <104899902+eduardojc-met@users.noreply.github.com>
2022-05-26 14:33:44 +0200

    Update back-configuration.groovy

--- back-configuration.groovy
+++ back-configuration.groovy
@@ -57,13 +57,13 @@ dir("C:/Program Files/IBM/Cloud/bin"){
               def datas = readYaml file:"Deployment_mgateway-fra.yml"
                datas.metadata["name"]="${appName}"+'-test'
                datas.metadata.labels["run"]="${appName}"+'-test'
-               datas[0].spec.selector.matchLabels=['app.kubernetes.io/component': "${appName}"+'-test', 'app.kubernetes.io/instance' : "${appName}"+'-test' ]
-               datas[0].spec.template.spec.template.metadata.labels=['app.kubernetes.io/component': "${appName}"+'-test', 'app.kubernetes.io/instance' : "${appName}"+'-test', 'environment':'develop','run':"${appName}"+'-test' ]
-                datas[0].spec.template.spec.template.spec.containers.name="${appName}"+'-test'
-                datas[0].spec.template.spec.template.spec.containers.image='de.icr.io/devops-tools/'+"${appName}"+'-test:'+"${pomVersion}"
-                datas[0].spec.template.spec.template.spec.containers.envFrom.secretRef=["name":"${appName}"]
+               datas.spec.selector.matchLabels=['app.kubernetes.io/component': "${appName}"+'-test', 'app.kubernetes.io/instance' : "${appName}"+'-test' ]
+               datas.spec.template.spec.template.metadata.labels=['app.kubernetes.io/component': "${appName}"+'-test', 'app.kubernetes.io/instance' : "${appName}"+'-test', 'environment':'develop','run':"${appName}"+'-test' ]
+                datas.spec.template.spec.template.spec.containers.name="${appName}"+'-test'
+                datas.spec.template.spec.template.spec.containers.image='de.icr.io/devops-tools/'+"${appName}"+'-test:'+"${pomVersion}"
+                datas.spec.template.spec.template.spec.containers.envFrom.secretRef=["name":"${appName}"]
                 bat 'del Deployment_mgateway-fra.yml'
-                writeYaml file: 'Deployment_mgateway-fra.yml', data: datas[0]
+                writeYaml file: 'Deployment_mgateway-fra.yml', data: datas
  dir("C:/Program Files/IBM/Cloud/bin"){
              bat label: 'Login to ibmcloud', script: '''ibmcloud.exe login -u %IBM_ACCESS_KEY_ID% -p %IBM_SECRET_ACCESS_KEY% -r eu-de ''' 
            bat label: 'Login to ibm cr', script: '''ibmcloud.exe  cr login '''
