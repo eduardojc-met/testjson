b2346e45efba74525b4697be35bf17b6b1095b2c
eduardojc-met <104899902+eduardojc-met@users.noreply.github.com>
2022-05-27 11:52:16 +0200

    Update mgateway-nld-conf.groovy

--- mgateway-nld-conf.groovy
+++ mgateway-nld-conf.groovy
@@ -124,17 +124,21 @@ service.spec.selector=['environment': 'develop','run':"${appName}"+'-test']
 
 
 
- /*
+ 
  dir("C:/Program Files/IBM/Cloud/bin"){
              bat label: 'Login to ibmcloud', script: '''ibmcloud.exe login -u %IBM_ACCESS_KEY_ID% -p %IBM_SECRET_ACCESS_KEY% -r eu-de ''' 
            bat label: 'Login to ibm cr', script: '''ibmcloud.exe  cr login '''
           bat label: 'Configuring kubernetes', script: '''ibmcloud.exe ks cluster config -c c7pb9jff0n7t4elurev0
  '''}
-              bat 'kubectl apply -f Deployment_mgateway-nld.yml --namespace=microgateway'
+             bat 'kubectl apply -f Deployment_mgateway-nld.yml --namespace=microgateway'
+               bat label: 'Install ibmcloud ks plugin', script: '''echo n | ibmcloud.exe plugin install container-service ''' 
+            bat label: 'Install ibmcloud cr plugin', script: '''echo n | ibmcloud.exe  plugin install container-registry ''' 
+           
+              
                bat 'kubectl apply -f Service_mgateway-nld.yml --namespace=microgateway'
                bat 'kubectl apply -f Ingress_mgateway-nld.yml --namespace=microgateway'
                bat 'kubectl apply -f Secret_mgateway-nld.yml --namespace=microgateway'
-            */
+           
   }
           
         
