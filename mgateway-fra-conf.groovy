e298afca3b9dd2e7310bb1c10519aeb464034469
eduardojc-met <104899902+eduardojc-met@users.noreply.github.com>
2022-05-27 09:01:38 +0200

    ,

--- mgateway-fra-conf.groovy
+++ mgateway-fra-conf.groovy
@@ -58,7 +58,7 @@ dir("C:/Program Files/IBM/Cloud/bin"){
          datas.metadata.labels["run"]="${appName}"+'-test'
           datas.spec.selector.matchLabels=['app.kubernetes.io/component': "${appName}"+'-test', 'app.kubernetes.io/instance' : "${appName}"+'-test' ]
         datas.spec.template.metadata.labels=['app.kubernetes.io/component': "${appName}"+'-test', 'app.kubernetes.io/instance' : "${appName}"+'-test', 'environment':'develop','run':"${appName}"+'-test' ]
-         datas.spec.template.spec.containers[0]["name"]="${appName}"
+         datas.spec.template.spec.containers[0]["name"]="${appName}"+'-test'
          datas.spec.template.spec.containers[0]["image"]='de.icr.io/devops-tools/'+"${appName}"+'-test:'+"${pomVersion}"
 datas.spec.template.spec.containers[0]["envFrom"][0]["configMapRef"]=["name":"${appName}"+'-test']        
 datas.spec.template.spec.containers[0]["envFrom"][1]["secretRef"]=["name":"${appName}"+'-test']        
@@ -118,15 +118,18 @@ service.spec.selector=['environment': 'develop','run':"${appName}"+'-test']
 
 
 
-/*
+
  
  dir("C:/Program Files/IBM/Cloud/bin"){
              bat label: 'Login to ibmcloud', script: '''ibmcloud.exe login -u %IBM_ACCESS_KEY_ID% -p %IBM_SECRET_ACCESS_KEY% -r eu-de ''' 
            bat label: 'Login to ibm cr', script: '''ibmcloud.exe  cr login '''
            bat label: 'Configuring kubernetes', script: '''ibmcloud.exe ks cluster config -c c7pb9mkf09cf7vh8tmu0
  '''}
-             //   bat 'kubectl apply -f back.yaml --namespace=develop'
-             */
+             //   bat 'kubectl apply -f Deployment_mgateway-fra.yml --namespace=develop'
+             //   bat 'kubectl apply -f Service_mgateway-fra.yml --namespace=develop'
+             //   bat 'kubectl apply -f Ingress_mgateway-fra.yml --namespace=develop'
+             //   bat 'kubectl apply -f Secret_mgateway-fra.yml --namespace=develop'
+             
   }
           
         
