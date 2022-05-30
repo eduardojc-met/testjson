f3da3855874ec02911a8726ce63a8077a4d192a5
eduardojc-met <104899902+eduardojc-met@users.noreply.github.com>
2022-05-30 13:45:39 +0200

    Update mgateway-nld-conf.groovy

--- mgateway-nld-conf.groovy
+++ mgateway-nld-conf.groovy
@@ -52,7 +52,7 @@ dir("C:/Program Files/IBM/Cloud/bin"){
            
          }
  bat 'docker push de.icr.io/devops-tools/'+"${appName}"+'-test:'+"${appVersion}"
-//  bat 'docker inspect '+"${appName}"+'-test:'+"${appVersion}"+' > dockerpushid.json'
+//  bat 'docker inspect de.icr.io/devops-tools/'+"${appName}"+'-test:'+"${appVersion}"+' > dockerpushid.json'
   //     def packageJSON = readJSON file: 'dockerpushid.json' 
     //    docker_push_id = packageJSON[0].Id.toString().replace("sha256:","")
     
@@ -76,10 +76,10 @@ dir("C:/Program Files/IBM/Cloud/bin"){
        def datas = readYaml file:"Deployment_mgateway-nld.yml"
         datas.metadata["name"]="${appName}"+'-test'
          datas.metadata.labels["run"]="${appName}"+'-test'
-         datas.metadata.annontations["last-image-push-id"]=docker_push_id
+         //datas.metadata.annontations["last-image-push-id"]=docker_push_id
           
        
-         datas.metadata.annontations["last-commit-sha"]=git_commit
+        // datas.metadata.annontations["last-commit-sha"]=git_commit
 
           datas.spec.selector.matchLabels=['app.kubernetes.io/component': "${appName}"+'-test', 'app.kubernetes.io/instance' : "${appName}"+'-test' ]
         datas.spec.template.metadata.labels=['app.kubernetes.io/component': "${appName}"+'-test', 'app.kubernetes.io/instance' : "${appName}"+'-test', 'environment':'microgateway','run':"${appName}"+'-test' ]
