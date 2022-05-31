afad2262cb7c5ce2720826790ed38a3c028f987a
eduardojc-met <104899902+eduardojc-met@users.noreply.github.com>
2022-05-31 09:35:37 +0200

    Update mgateway-nld-conf.groovy

--- mgateway-nld-conf.groovy
+++ mgateway-nld-conf.groovy
@@ -53,7 +53,7 @@ dir("C:/Program Files/IBM/Cloud/bin"){
          }
  bat 'docker push de.icr.io/devops-tools/'+"${appName}"+'-test:'+"${appVersion}"
  docker inspect --format='{{.RepoDigests}}'
- bat 'docker inspect --format="{{.RepoDigests}}" de.icr.io/devops-tools/'+"${appName}"+'-test:'+"${appVersion}"+' > dockerpushid.json'
+ bat "docker inspect --format='{{.RepoDigests}}' de.icr.io/devops-tools/"+"${appName}"+'-test:'+"${appVersion}"+' > dockerpushid.json'
  def packageJSON = readJSON file: 'dockerpushid.json' 
 docker_push_id = packageJSON[0].Id.toString().replace('de.icr.io/devops-tools/'+"${appName}"+'-test'+"@sha256","").replace("[","").replace("]","")
     
